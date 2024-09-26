cc238e81cd260388924cedd6f481b5da59547586
BeEnAyA <106618310+BeEnAyA@users.noreply.github.com>
2024-09-26 13:39:39 +0545

    Retry to Deploy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'drizzle-kit'
+import "dotenv/config"
 
 if (!process.env.DB_HOST || !process.env.DB_PORT || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_DATABASE) {
 	throw new Error('Database credentials are missing')
